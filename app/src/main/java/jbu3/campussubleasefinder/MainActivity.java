48c9276835457194874dc834321eb7c88282ca3f
Stephen Hurwit <10472810+shurwit@users.noreply.github.com>
2019-11-18 19:23:30 -0600

    linked and fixed some issues

--- app/src/main/java/jbu3/campussubleasefinder/MainActivity.java
+++ app/src/main/java/jbu3/campussubleasefinder/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
@@ -12,6 +13,8 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -23,7 +26,8 @@ public class MainActivity extends AppCompatActivity implements BuildingRecyclerV
         add(new Building("123 Example St.", 4, 2, 4.5));
     }};
 
-    private BuildingRecyclerViewAdapter buildingAdapter;
+    private BuildingRecyclerViewAdapter buildingsAdapter;
+    private FloatingActionButton postSubleaseFab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,12 +37,20 @@ public class MainActivity extends AppCompatActivity implements BuildingRecyclerV
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_account_circle_white_24dp);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        RecyclerView buildingsRecyclerView = findViewById(R.id.main_building_recycler_view);
+        buildingsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        buildingsAdapter = new BuildingRecyclerViewAdapter(this, buildings);
+        buildingsAdapter.setClickListener(this);
+        buildingsRecyclerView.setAdapter(buildingsAdapter);
 
-        RecyclerView recyclerView = findViewById(R.id.main_building_recycler_view);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        buildingAdapter = new BuildingRecyclerViewAdapter(this, buildings);
-        buildingAdapter.setClickListener(this);
-        recyclerView.setAdapter(buildingAdapter);
+        postSubleaseFab = findViewById(R.id.add_sublease_fab);
+        postSubleaseFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent postSubleaseIntent = new Intent(getApplicationContext(), PostSubleaseActivity.class);
+                startActivity(postSubleaseIntent);
+            }
+        });
     }
 
     @Override
@@ -53,6 +65,8 @@ public class MainActivity extends AppCompatActivity implements BuildingRecyclerV
         // Handle item selection
         switch (item.getItemId()) {
             case android.R.id.home:
+                Intent showProfileIntent = new Intent(this, ProfileActivity.class);
+                startActivity(showProfileIntent);
                 return true;
             case R.id.filters:
                 return true;
