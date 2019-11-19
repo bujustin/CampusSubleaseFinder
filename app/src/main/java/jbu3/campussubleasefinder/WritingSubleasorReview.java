462acdf6829395cb688875a0c7f3567b98a36717
Alex5414 <32084250+Alex5414@users.noreply.github.com>
2019-11-19 16:24:55 -0600

    reviewAndFilter

---
+++ app/src/main/java/jbu3/campussubleasefinder/WritingSubleasorReview.java
@@ -0,0 +1,14 @@
+package jbu3.campussubleasefinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WritingSubleasorReview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_writing_subleasor_review);
+    }
+}
