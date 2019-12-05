f6c2b1c09e817682de722d3acd8dc725b7070f66
Stephen Hurwit <10472810+shurwit@users.noreply.github.com>
2019-12-05 16:36:14 -0600

    adding user details

--- app/src/main/java/jbu3/campussubleasefinder/models/User.java
+++ app/src/main/java/jbu3/campussubleasefinder/models/User.java
@@ -1,15 +1,28 @@
 package jbu3.campussubleasefinder.models;
 
+import java.util.ArrayList;
+
 public class User {
     public int id;
     public String name;
-    public int numConnections;
+    public String about;
+    public String email;
+    public String phone;
     public double rating;
+    public ArrayList<Sublease> subleases;
+    public ArrayList<Review> reviews;
+    public ArrayList<User> connections;
+    public int[] connectionIDs;
 
-    public User(int id, String name, int numConnections, double rating) {
+    public User(int id, String name, String email, String phone, String about, int[] connectionIDs) {
         this.id = id;
         this.name = name;
-        this.numConnections = numConnections;
-        this.rating = rating;
+        this.email = email;
+        this.phone = phone;
+        this.about = about;
+        this.connectionIDs = connectionIDs;
+        this.subleases = new ArrayList<>();
+        this.reviews = new ArrayList<>();
+        this.connections = new ArrayList<>();
     }
 }
