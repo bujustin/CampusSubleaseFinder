4fa7b0abca93da63e4d51eb5980a5df7e2f84d64
jiahuahe <jiahuah2@illinois.edu>
2019-12-05 21:02:28 -0600

    upload photo function in post a sublease

--- app/src/main/java/jbu3/campussubleasefinder/activities/PostSubleaseActivity.java
+++ app/src/main/java/jbu3/campussubleasefinder/activities/PostSubleaseActivity.java
@@ -3,11 +3,15 @@ package jbu3.campussubleasefinder.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.Calendar;
@@ -26,6 +30,19 @@ public class PostSubleaseActivity extends AppCompatActivity {
     int month;
     int dayOfMonth;
     Calendar calendar;
+
+    private static final int RESULT_LOAD_IMAGE = 1;
+    private ImageView imageToUpload;
+    private Button bUploadImage;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();
+            imageToUpload.setImageURI(selectedImage);
+        }
+    }
 
 
     @Override
@@ -74,6 +91,25 @@ public class PostSubleaseActivity extends AppCompatActivity {
                 }, year, month, dayOfMonth);
                 datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis());
                 datePickerDialog.show();
+            }
+        });
+
+        imageToUpload = findViewById(R.id.ImageToUpload);
+
+        Button bUploadImage = findViewById(R.id.bUploadImage);
+
+        imageToUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
+            }
+        });
+        bUploadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
             }
         });
     }
--- app/src/main/java/jbu3/campussubleasefinder/activities/WriteReviewActivity.java
+++ app/src/main/java/jbu3/campussubleasefinder/activities/WriteReviewActivity.java
@@ -15,54 +15,5 @@ import android.widget.ImageView;
 import jbu3.campussubleasefinder.R;
 
 public class WriteReviewActivity extends AppCompatActivity {
-    private static final int RESULT_LOAD_IMAGE = 1;
-    private ImageView imageToUpload;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_write_review);
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        imageToUpload = findViewById(R.id.ImageToUpload);
-
-        Button bUploadImage = findViewById(R.id.bUploadImage);
-
-        imageToUpload.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
-            }
-        });
-        bUploadImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
-            }
-        });
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle item selection
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                finish();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null) {
-            Uri selectedImage = data.getData();
-            imageToUpload.setImageURI(selectedImage);
-        }
-    }
 }
